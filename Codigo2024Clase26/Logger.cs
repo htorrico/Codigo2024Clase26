0a7e3792cbd28ef74703ff4e285b5f2bc4cc0370
Hugo Torrico <torricomarquezh@gmail.com>
2024-11-04 21:57:49 -0500

    Patron Singleton

---
+++ Codigo2024Clase26/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo2024Clase26
+{
+    public class Logger
+    {
+        private string _logFilePath = "log.txt";
+
+
+        // Constructor público que permite crear múltiples instancias.
+        public Logger()
+        {
+            if (!File.Exists(_logFilePath))
+            {
+                File.Create(_logFilePath).Dispose();
+            }
+        }
+
+
+        private static Logger _instance;
+        public static Logger Instance
+        {
+            get
+            {
+                
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                    }
+                    return _instance;                
+            }
+        }
+
+
+
+
+
+
+        // Método para registrar un mensaje en el archivo.
+        public void Log(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(_logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+    }
+}
--- Codigo2024Clase26/Program.cs
+++ Codigo2024Clase26/Program.cs
@@ -28,6 +28,7 @@ void ClonarError()
 
 
 
+    Console.WriteLine(ReferenceEquals(producto, producto1));
 
     Console.WriteLine(producto.Nombre); //TV SAMSUMNG
     Console.WriteLine(producto1.Nombre); //TV LG
@@ -42,34 +43,134 @@ void ClonarError()
     Console.Read();
 }
 
+void ClonarCorrecto()
+{
+    Persona persona = new Persona("Hugo", "Torrico", "", "", 36);
+    Console.WriteLine(persona.Nombres + " " + persona.Apellidos);
+
+    Persona persona1 = persona.ClonarManual();
+    Console.WriteLine(persona1.Nombres + " " + persona1.Apellidos);
+
+    Persona persona2 = persona.Clonar();
+    Console.WriteLine(persona2.Nombres + " " + persona2.Apellidos);
 
 
-Persona persona = new Persona("Hugo", "Torrico","","",36);
-Console.WriteLine(persona.Nombres + " " + persona.Apellidos);
+    Console.WriteLine("===========================================");
 
-Persona persona1 = persona.ClonarManual();
-Console.WriteLine(persona1.Nombres + " " + persona1.Apellidos);
+    persona1.Apellidos = "Marquez";
+    persona1.Nombres = "Felipe";
 
-
-Persona persona2 = persona.Clonar();
-Console.WriteLine(persona2.Nombres + " " + persona2.Apellidos);
+    persona2.Apellidos = "xxxx";
+    persona2.Nombres = "yyyyy";
 
 
+    Console.WriteLine(persona.Nombres + " " + persona.Apellidos);
+    Console.WriteLine(persona1.Nombres + " " + persona1.Apellidos);
+    Console.WriteLine(persona2.Nombres + " " + persona2.Apellidos);
+
+
+}
 
 
 
-Console.WriteLine("===========================================");
+void Prueba1()
+{
+    Logger logger11 = new Logger();
+    logger11.Log("Inicio de la aplicación.");
+}
 
-persona1.Apellidos = "Marquez";
-persona1.Nombres = "Felipe";
+void Prueba2()
+{
+    Logger logger22 = new Logger();
+    logger22.Log("Inicio de la aplicación.");
+}
 
-persona2.Apellidos = "xxxx";
-persona2.Nombres = "yyyyy";
+
+void PruebaSingleton1()
+{
+    Logger logger11 = Logger.Instance;
+    logger11.Log("Inicio de la aplicación.");
+}
+
+void PruebaSingleton2()
+{
+    Logger logger22 = Logger.Instance;
+    logger22.Log("Inicio de la aplicación.");
+}
+
+void PruebaSingleton3()
+{
+    Logger logger33 = Logger.Instance;
+    logger33.Log("Inicio de la aplicación.");
+}
+
+PruebaSingleton1();
+PruebaSingleton2();
+PruebaSingleton3();
 
 
-Console.WriteLine(persona.Nombres + " " + persona.Apellidos);
-Console.WriteLine(persona1.Nombres + " " + persona1.Apellidos);
-Console.WriteLine(persona2.Nombres + " " + persona2.Apellidos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ClonarError();
+
+
+//Logger logger1 = new Logger();
+//logger1.Log("Inicio de la aplicación.");
+
+//Logger logger2 = new Logger();
+//logger2.Log("Otra operación en la aplicación.");
+
+
+//Logger logger3 = new Logger();
+//logger2.Log("Otra operación en la aplicación.");
+
+
+//Logger logger4 = new Logger();
+//logger2.Log("Otra operación en la aplicación.");
+
+//Logger logger5 = new Logger();
+//logger2.Log("Otra operación en la aplicación.");
+
+//// Las instancias son diferentes, lo que significa que podría haber inconsistencias si se usan múltiples instancias.
+//Console.WriteLine(ReferenceEquals(logger1, logger2)); // Imprime "False"
+
+
+
+//Logger logger11 = Logger.Instance;
+//logger11.Log("Inicio de la aplicación.");
+
+//Logger logger22 = Logger.Instance;
+//logger22.Log("Otra operación en la aplicación.");
+
+
+//Logger logger33 = Logger.Instance;
+//logger33.Log("Otra operación en la aplicación.");
+
+
+
+//Logger logger1 = new Logger();
+//logger1.Log("Inicio de la aplicación.");
+
+//logger1.Log("Otra operación en la aplicación.");
+
+//logger1.Log("Otra operación en la aplicación.");
+
+
+
+
+
+
 
 
 Console.Read();
